178f02ae88be7aae385a0bde660d739ad049cb23
nyubachi <nyubachi248@gmail.com>
2019-01-12 16:21:53 +0900

    bug fix

--- R/stdwtcalc.R
+++ R/stdwtcalc.R
@@ -1,10 +1,9 @@
 #' Calculate standard weight
 #'
 #' @param height Unit is centimeter
-#' @param weight Unit is kilogram
 #'
 #' @export
 #'
-stdwtcalc <- function(height, weight) {
+stdwtcalc <- function(height) {
   round((height / 100)^2 * 22, digits = 2)
 }
